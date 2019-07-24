225a0fdabdd4f7294ac646c5eaa2506e72625b6e
Cintia <cinmts@gmail.com>
2019-07-24 10:50:16 -0300

    Alterando testes para usar a anotaçáo @Before

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import java.time.LocalTime;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
@@ -14,43 +15,46 @@ import br.com.caelum.ingresso.model.Sessao;
 
 public class DescontoTest {
 
+	private Sala sala;
+	private Filme filme;
+	private Sessao sessao;
+	private Ingresso ingressoSemDesconto;
+	private Ingresso descontoParaBancos;
+	private Ingresso descontoParaEstudantes;
+
+	@Before
+	public void preparaDescontos() {
+
+		this.sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
+		this.filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
+		this.sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		this.ingressoSemDesconto = new Ingresso(sessao, new SemDesconto());
+		this.descontoParaBancos = new Ingresso(sessao, new DescontoParaBancos());
+		this.descontoParaEstudantes = new Ingresso(sessao, new DescontoParaEstudantes());
+	}
+
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
 
-		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
-
 		BigDecimal precoEsperado = new BigDecimal("32.50");
 
-		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+		Assert.assertEquals(precoEsperado, this.ingressoSemDesconto.getPreco());
 	}
 
 	@Test
 	public void deveConcederDescontoDe30PorCentoParaClientesDeBancos() {
 
-		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
-
 		BigDecimal precoEsperado = new BigDecimal("22.75");
 
-		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+		Assert.assertEquals(precoEsperado, descontoParaBancos.getPreco());
 	}
-	
+
 	@Test
 	public void deveConcederDescontoDe50PorCentoParaEstudantes() {
 
-		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
-
 		BigDecimal precoEsperado = new BigDecimal("16.25");
 
-		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+		Assert.assertEquals(precoEsperado, descontoParaEstudantes.getPreco());
 	}
-	
+
 }
