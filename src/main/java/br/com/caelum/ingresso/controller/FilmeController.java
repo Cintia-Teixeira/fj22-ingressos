5edfacdfc33b34f6b4463aa0e6b920e48032840c
Cintia <cinmts@gmail.com>
2019-07-24 12:48:16 -0300

    Criando meios para disponibilizaçáo de detalhes sobre os filmes.

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -18,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.OmdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -37,10 +39,17 @@ public class FilmeController {
 		Filme filme = filmeDao.findOne(id);
 		List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
 		
+		Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
+		
 		modelAndView.addObject("sessoes", sessoes);
+		modelAndView.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
 		
 		return modelAndView;
 	}
+	
+
+    @Autowired
+    private OmdbClient client;
 
     @Autowired
     private FilmeDao filmeDao;
